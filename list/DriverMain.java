925b2a1bcf01d4f63f2e6f3b31f3ef3db3825063
mehraUTD <mehraUTD@users.noreply.github.com>
2025-10-20 13:37:06 -0500

    Create DriverMain.java

---
+++ list/DriverMain.java
@@ -0,0 +1,54 @@
+package list;
+
+
+public class DriverMain {
+
+	public static void main(String[] args) {
+//		MyArrayList myList = new MyArrayList();
+		MyLinkedList myList = new MyLinkedList();
+		myList.print();
+		myList.addFirst("h");
+		myList.addFirst("e");
+		myList.addFirst("l");
+		myList.addFirst("l");
+		myList.print();
+		myList.addLast("o");
+		myList.addLast("a");
+		myList.addLast("b");
+		myList.addLast("c");
+		myList.addLast("d");
+		myList.addLast("e");
+		myList.addLast("f");
+		myList.addLast("g");
+		myList.addLast("h");
+		myList.print();
+//		myList.removeLast();
+//		myList.removeLast();
+//		myList.removeLast();
+//		myList.removeLast();
+//		myList.print();
+//		myList.removeLast();
+//		myList.print();
+//		myList.removeLast();
+//		myList.print();
+//		myList.removeLast();
+//		myList.print();
+//		myList.removeLast();
+//		myList.print();
+//		myList.removeLast();
+//		myList.print();
+//		myList.removeLast();
+//		myList.print();
+//		myList.removeLast();
+//		myList.print();
+//		myList.addFirst("a", 0);
+//		myList.print();
+//		myList.addFirst("b", 0);
+//		myList.print();
+//		myList.addFirst("c", 0);
+//		myList.print();
+//		myList.addFirst("d", 0);
+//		myList.print();
+
+	}
+}
