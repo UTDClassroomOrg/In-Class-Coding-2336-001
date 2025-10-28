9359fd387d2abf3dbf542374abe0de389a44b48c
mehraUTD <mehraUTD@users.noreply.github.com>
2025-10-28 17:57:38 -0500

    Update DriverMain.java

--- list/DriverMain.java
+++ list/DriverMain.java
@@ -1,31 +1,30 @@
 package list;
 
-
 public class DriverMain {
 
 	public static void main(String[] args) {
-//		MyArrayList myList = new MyArrayList();
-		MyLinkedList myList = new MyLinkedList();
-		myList.print();
-		myList.addFirst("h");
-		myList.addFirst("e");
-		myList.addFirst("l");
-		myList.addFirst("l");
-		myList.print();
-		myList.addLast("o");
-		myList.addLast("a");
-		myList.addLast("b");
-		myList.addLast("c");
-		myList.addLast("d");
-		myList.addLast("e");
-		myList.addLast("f");
-		myList.addLast("g");
-		myList.addLast("h");
-		myList.print();
-//		myList.removeLast();
-//		myList.removeLast();
-//		myList.removeLast();
-//		myList.removeLast();
+////		MyArrayList myList = new MyArrayList();
+//		MyLinkedList myList = new MyLinkedList();
+//		myList.print();
+//		myList.addFirst("h");
+//		myList.addFirst("e");
+//		myList.addFirst("l");
+//		myList.addFirst("l");
+//		myList.print();
+//		myList.addLast("o");
+//		myList.addLast("a");
+//		myList.addLast("b");
+//		myList.addLast("c");
+//		myList.addLast("d");
+//		myList.addLast("e");
+//		myList.addLast("f");
+//		myList.addLast("g");
+//		myList.addLast("h");
+//		myList.print();
+//		myList.removeFirst();
+//		myList.removeFirst();
+//		myList.removeFirst();
+//		myList.removeFirst();
 //		myList.print();
 //		myList.removeLast();
 //		myList.print();
@@ -35,20 +34,54 @@ public class DriverMain {
 //		myList.print();
 //		myList.removeLast();
 //		myList.print();
-//		myList.removeLast();
-//		myList.print();
-//		myList.removeLast();
-//		myList.print();
-//		myList.removeLast();
-//		myList.print();
-//		myList.addFirst("a", 0);
-//		myList.print();
-//		myList.addFirst("b", 0);
-//		myList.print();
-//		myList.addFirst("c", 0);
-//		myList.print();
-//		myList.addFirst("d", 0);
-//		myList.print();
+////		myList.remove("b");
+////		myList.print();
+////		myList.remove("a");
+////		myList.print();
+////		myList.remove("c");
+////		myList.print();
+////		myList.remove("d");
+////		myList.print();
+////		myList.remove("o");
+////		myList.print();
+//		System.out.println(myList.find("5"));
+//		System.out.println(myList.find("d"));
+//		System.out.println(myList.getSize());
+//		System.out.println(myList.getElement(6));
 
+		
+		//MyStackOfArrayList mystack = new MyStackOfArrayList();
+		MyStack mystack = new MyStack();
+		mystack.push("a");
+		mystack.print();
+		mystack.push("b");
+		mystack.print();
+		mystack.push("c");
+		mystack.print();
+		System.out.println(mystack.peek());
+		mystack.pop();
+		mystack.print();
+		System.out.println(mystack.peek());
+		mystack.pop();
+		mystack.print();
+		System.out.println(mystack.peek());
+		mystack.pop();
+		mystack.print();
+		System.out.println(mystack.peek());
+		
+//		MyQueue myQueue = new MyQueue();
+//		myQueue.enqueue("a");
+//		myQueue.print();
+//		myQueue.enqueue("b");
+//		myQueue.print();
+//		myQueue.enqueue("c");
+//		myQueue.print();
+//		myQueue.dequeue();
+//		myQueue.print();
+//		myQueue.dequeue();
+//		myQueue.print();
+//		myQueue.dequeue();
+//		myQueue.print();
+			
 	}
 }
