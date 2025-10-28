d81364469864a757eb65a20c5fc1323ccbec67c8
mehraUTD <mehraUTD@users.noreply.github.com>
2025-10-28 17:56:39 -0500

    Create MyStackOfArrayList.java

---
+++ list/MyStackOfArrayList.java
@@ -0,0 +1,38 @@
+package list;
+/*
+ * Stack is a LIFO (FILO)
+ * A special case of MyArrayList
+ */
+public class MyStackOfArrayList {
+	private MyArrayList myStack = new MyArrayList(); //composition relationship
+	private int top;
+
+	public boolean isEmpty() {
+		return myStack.isEmpty(); //forward
+	}
+	
+	public void push(String s) {
+		myStack.addLast(s);
+		top = myStack.getSize() ;
+	}
+	
+	public void pop() {
+		myStack.removeLast();
+		top = myStack.getSize() ;
+	}
+	
+	public String peek() {
+		if(!isEmpty())
+			return myStack.getElement(top - 1);
+		else
+			return "Stack is empty!";
+	}
+	
+	public int search(String key) {
+		return myStack.find(key);
+	}
+	
+	public void print() {
+		myStack.print();
+	}
+}
